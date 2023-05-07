b9819b493e9b491b183fbe3edd5469ced3694a9f
Vincent Alfarieco <vincent111232@gmail.com>
2023-05-07 18:22:43 +0700

    tambah animasi navbar

--- script/index.js
+++ script/index.js
@@ -1,3 +1,17 @@
+const navbar = document.querySelector('.navbar');
+
+document.addEventListener('scroll', () =>{
+    if(window.scrollY > 0 ){
+        navbar.classList.add('scroll')
+    }else{
+        navbar.classList.remove('scroll')
+    }
+})
+
+
+
+
+
 const redirectToPage = (link) =>{
     window.location.href=link
 }
