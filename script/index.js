831f8102fd27df28d20b722191f6d199e599c3bd
Moch Rizqi <rizqia417@gmail.com>
2023-05-11 19:44:24 +0700

    Buat Order Form

--- script/index.js
+++ script/index.js
@@ -22,3 +22,40 @@ function buttonActive(active, inactive) {
     document.getElementById(active).className="active"
     document.getElementById(inactive).className=""
 }
+
+function vehicleSelection() {
+    var tipe = document.getElementById('tipe').value
+    if (tipe === "1") {
+        document.getElementById('mobil-self').style.display = 'none'
+        document.getElementById('mobil-driver').style.display = 'none'
+        document.getElementById('motor').style.display = 'block'
+    } 
+    if (tipe === "2") {
+        document.getElementById('mobil-self').style.display = 'block'
+        document.getElementById('mobil-driver').style.display = 'none'
+        document.getElementById('motor').style.display = 'none'
+    } 
+    if (tipe === "3") {
+        document.getElementById('mobil-self').style.display = 'none'
+        document.getElementById('mobil-driver').style.display = 'block'
+        document.getElementById('motor').style.display = 'none'
+    }
+    if (tipe === "0") {
+        document.getElementById('mobil-self').style.display = 'none'
+        document.getElementById('mobil-driver').style.display = 'none'
+        document.getElementById('motor').style.display = 'none'
+    }
+}
+
+function hitungDurasi() {
+    var dari = new Date(document.getElementById('dari').value);
+    var sampai = new Date(document.getElementById('sampai').value);
+  
+    var durasi = Math.abs(sampai.getTime() - dari.getTime()) / (1000 * 3600 * 24);
+  
+    document.getElementById('hari').textContent = durasi + ' hari';
+  }
+  
+  document.getElementById('dari').addEventListener('change', hitungDurasi);
+  document.getElementById('sampai').addEventListener('change', hitungDurasi);
+  
